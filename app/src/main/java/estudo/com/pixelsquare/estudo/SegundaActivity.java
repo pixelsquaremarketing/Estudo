5a259c22877aa03d37c3c28f66a06ed9c120614c
pixelsquaremarketing <estudo@pixelsquaremarketing.com>
2016-10-28 11:16:24 -0200

    Passando Parametros para Segunda Activity
    Passando uma string msg para a segunda activity vinda da primeira
    activity.

--- app/src/main/java/estudo/com/pixelsquare/estudo/SegundaActivity.java
+++ app/src/main/java/estudo/com/pixelsquare/estudo/SegundaActivity.java
@@ -2,6 +2,7 @@ package estudo.com.pixelsquare.estudo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class SegundaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        String msg = getIntent().getStringExtra("msg");
+
+        TextView txt = (TextView) findViewById(R.id.txt);
+
+        txt.setText(msg);
     }
 }
